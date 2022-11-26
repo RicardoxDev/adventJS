0feab404f312df5a7f89ac53ff99f111712a7c0e
Ricardo Martinez <ricardoxdev@gmail.com>
2022-11-26 15:54:14 -0400

    Add challenge-15

--- 2021/challenge-15/challenge-15.js
+++ 2021/challenge-15/challenge-15.js
@@ -2,3 +2,12 @@ function checkSledJump(heights) {
   // ¡No olvides compartir tu solución en redes!
   return false
 }
+
+checkSledJump([1, 2, 3, 2, 1]) // true: sube y baja de forma estricta
+checkSledJump([0, 1, 0]) // -> true: sube y baja de forma estricta
+checkSledJump([0, 3, 2, 1]) // -> true: sube y baja de forma estricta
+checkSledJump([0, 1000, 1]) // -> true: sube y baja de forma estricta
+
+checkSledJump([2, 4, 4, 6, 2]) // false: no sube de forma estricta
+checkSledJump([1, 2, 3]) // false: sólo sube
+checkSledJump([1, 2, 3, 2, 1, 2, 3]) // false: sube y baja y sube... ¡no vale!
